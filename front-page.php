7f2de9f0c5ed547667a15e928122a6e3075e935a
Руньковский Константин <raitok@mail.ru>
2021-03-03 18:49:02 +0300

    lesson 5

--- front-page.php
+++ front-page.php
@@ -10,18 +10,18 @@
 
                 $myposts = get_posts([ 
                     'numberposts' => 1,
-                    'category_name' => 'javascript'
                 ]);
 
                 if( $myposts ){
                     foreach( $myposts as $post ){
                         setup_postdata( $post );
                         ?>
+
                         <article class="hero__left topic">
-                            <img src="<?php the_post_thumbnail_url(); ?>" alt="Картинка к посту" class="topic__thumb">
-                            <?php $author_id = get_the_author_meta( 'ID' ) ?>
-                            <a href=" <?php echo get_author_posts_url( $author_id ) ?>" class="topic-author">
-                                <img class="topic-author__avatar" src="<?php echo get_avatar_url( $author_id ) ?>" alt="Аватар пользователя">
+                            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="topic__thumb">
+                            <?php $author_id = get_the_author_meta( 'ID' ); ?>
+                            <a href=" <?php echo get_author_posts_url( $author_id ); ?>" class="topic-author">
+                                <img class="topic-author__avatar" src="<?php echo get_avatar_url( $author_id ); ?>" alt="Аватар пользователя">
                                 <div class="topic-author__info">
                                     <h3 class="topic-author__name"><?php the_author(); ?></h3>
                                     <span class="topic-author__rank">Разработчик</span>
@@ -30,9 +30,10 @@
                             <div class="topic__info">
                                 <?php the_category(); ?>
                                 <h2 class="topic__heading"><?php the_title(); ?></h2>
-                                <a href="<?php echo get_the_permalink() ?>" class="topic__button">Читать далее</a>
+                                <a href="<?php the_permalink(); ?>" class="topic__button">Читать далее</a>
                             </div>
                         </article>
+                        
                         <?php 
                     }
                 } else {
@@ -43,6 +44,7 @@
             ?>
                 <div class="hero__right recomend">
                     <h3 class="recomend__heading">Рекомендуем</h3>
+                        <ul class="recomend__list">
                         <?php
                             global $post;
 
@@ -50,19 +52,18 @@
                                 'numberposts' => 5,
                                 'offset' => 1,
                             ]);
-
                             if( $myposts ){
                                 foreach( $myposts as $post ){
                                     setup_postdata( $post );
                                     ?>
-                                    <ul class="recomend__list">
+                                    
                                         <li class="recomend__item">
                                             <?php the_category(); ?>
-                                            <a href="<?php echo get_the_permalink() ?>" class="recomend__link">
-                                                <h4 class="recomend__text"><?php the_title(); ?></h4>
+                                            <a href="<?php the_permalink(); ?>" class="recomend__link">
+                                                <h4 class="recomend__text"><?php echo wp_trim_words( get_the_title(), 5); ?></h4>
                                             </a>
                                         </li>
-                                    </ul>
+
                                     <?php 
                                 }
                             } else {
@@ -71,11 +72,44 @@
 
                             wp_reset_postdata(); // Сбрасываем $post
                         ?>
+                        </ul>
                 </div>
             </div>
         </div>
     </section>
-</main>
+    <section class="articles">
+        <div class="container">
+            <ul class="articles__list">
+                <?php
+                    global $post;
 
+                    $myposts = get_posts([ 
+                        'numberposts' => 4,
+                    ]);
+
+                    if( $myposts ){
+                        foreach( $myposts as $post ){
+                            setup_postdata( $post );
+                            ?>
+
+                                <li class="articles__item">
+                                    <a href="<?php the_permalink(); ?>" class="articles__link">
+                                        <h3 class="articles__heading"><?php echo mb_strimwidth(get_the_title(), 0, 50, "..."); ?></h3>
+                                        <img src="<?php the_post_thumbnail_url('homepage-thumb'); ?>" alt="<?php the_title(); ?>" class="articles__img">
+                                    </a>
+                                </li>
+
+                            <?php 
+                        }
+                    } else {
+                        // Постов не найдено
+                    }
+
+                    wp_reset_postdata(); // Сбрасываем $post
+                ?>
+            </ul>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+// правильный способ подключить стили и скрипты
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+function enqueue_universal_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri(), null, time(), null );
+    wp_enqueue_style( 'universal-theme', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style', time(), null );
+}
 
 if ( ! function_exists( 'universal_theme_setup' ) ) :
 	function universal_theme_setup() {
@@ -30,9 +36,22 @@ endif;
 
 add_action( 'after_setup_theme', 'universal_theme_setup' );
 
-// правильный способ подключить стили и скрипты
-add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
-function enqueue_universal_style() {
-	wp_enqueue_style( 'style', get_stylesheet_uri(), null, time(), null );
-    wp_enqueue_style( 'universal-theme', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style', time(), null );
+
+## отключаем создание миниатюр файлов для указанных размеров
+
+add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
+function delete_intermediate_image_sizes( $sizes ){
+	// размеры которые нужно удалить
+	return array_diff( $sizes, [
+		'medium_large',
+		'large',
+		'1536x1536',
+		'2048x2048',
+	] );
+}
+
+// Добавляем миниатюры
+
+if ( function_exists( 'add_image_size' ) ) {
+	add_image_size( 'homepage-thumb', 65, 65, true ); 
 }
