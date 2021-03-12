a869dad1af7039f1a6dbe4dbfa8bdac0ed40c807
Руньковский Константин <raitok@mail.ru>
2021-03-12 17:43:44 +0300

    lesson 11

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+  });
--- front-page.php
+++ front-page.php
@@ -33,7 +33,12 @@
                             <div class="topic__info">
                                 <?php the_category(); ?>
                                 <h2 class="topic__heading"><?php the_title(); ?></h2>
-                                <a href="<?php the_permalink(); ?>" class="read-more-button">Читать далее</a>
+                                <a href="<?php the_permalink(); ?>" class="read-more-button topic__button">
+                                    <span>Читать далее</span>
+                                    <svg class="arrow-icon">
+                                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#arrow"></use>
+                                    </svg>
+                                </a>
                             </div>
                         </article>
                         
@@ -257,9 +262,14 @@
                 ?>
 
                 <section class="investigation" style="background: linear-gradient(0deg, rgba(64, 48, 61, 0.35), rgba(64, 48, 61, 0.35)), url('<?php the_post_thumbnail_url(); ?>') no-repeat center center">
-                    <div class="container">
+                    <div class="container investigation__container">
                         <h2 class="investigation__heading"><?php the_title(); ?></h2>
-                        <a href="<?php the_permalink(); ?>" class="read-more-button">Читать статью</a>
+                        <a href="<?php the_permalink(); ?>" class="read-more-button">
+                            <span>Читать статью</span>
+                            <svg class="arrow-icon">
+                                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#arrow"></use>
+                            </svg>
+                        </a>
                     </div>
                 </section>
 
@@ -329,6 +339,156 @@
                     <?php get_sidebar('articles'); ?>                    
                 </div>        
             </div>
+        </div>
+    </section>
+    
+    <!-- SPECIAL -->
+
+    <section class="special">
+        <div class="container special__container">
+            <div class="special__left photo-report">
+            <?php
+                    global $post;
+
+                    $myposts = get_posts([ 
+                        'numberposts' => 1,
+                        'category_name' => 'photoreport'
+                    ]);
+
+                    if( $myposts ){
+                        foreach( $myposts as $post ){
+                            setup_postdata( $post );
+                            ?>
+                            <!-- Slider main container -->
+                            <div class="swiper-container photo-report__slider">
+                            <!-- Additional required wrapper -->
+                                <div class="swiper-wrapper">
+                                    <?php
+                                    $images = get_attached_media('image');
+                                    foreach ($images as $image) {
+                                    ?>
+                                        <div class="swiper-slide photo-report__slide">
+                                            <img class="photo-report__img" src="<?php echo $image -> guid; ?>" alt="Картинка">
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                                <!-- If we need pagination -->
+                                <div class="swiper-pagination"></div>
+                            </div>
+                            <div class="photo-report__article">
+                                <?php 
+                                foreach( get_the_category() as $category ) {
+                                    printf(
+                                        '<a href="%s" class="photo-report__category">%s</a>',
+                                        esc_url(get_category_link($category)),
+                                        esc_html($category -> name),
+                                    ); 
+                                }
+                                $author_id = get_the_author_meta( 'ID' ); 
+                                ?>
+                                <a href=" <?php echo get_author_posts_url( $author_id ); ?>" class="photo-report__author">
+                                    <img class="photo-report__author-avatar" src="<?php echo get_avatar_url( $author_id ); ?>" alt="Аватар пользователя">
+                                    <h3 class="photo-report__author-name"><?php the_author(); ?></h3>
+                                    <span class="photo-report__author-rank">Разработчик</span>
+                                </a>
+                                <h2 class="photo-report__heading">
+                                    <a href="<?php the_permalink(); ?>" class="photo-report__link"><?php the_title(); ?></a>
+                                </h2>        
+                                <a href="<?php the_permalink(); ?>" class="read-more-button">
+                                    <svg class="photo-report__icon">
+                                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#images"></use>
+                                    </svg>
+                                    <span>Смотреть фото</span>
+                                    <span class="photo-report__count"><?php echo count($images); ?></span>
+                                </a>                                
+                            </div> 
+                            <?php 
+                        }
+                    } else {
+                        // Постов не найдено
+                    }
+
+                    wp_reset_postdata(); // Сбрасываем $post
+                ?>            
+            </div>
+    
+    <!-- Правая колонка -->
+
+            <ul class="special__right">
+                <?php
+                // задаем нужные нам критерии выборки данных из БД
+                $query = new WP_Query('category_name=career');
+
+                // Цикл
+                if ( $query->have_posts() ) {                   
+                    while ( $query->have_posts() ) {
+                        $query->the_post();
+                ?>
+    
+                <!-- Первый пост -->
+
+                <li class="special__article">
+                    <img class="special__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/career.png" alt="<?php the_title(); ?>">
+                    <?php 
+                    foreach( get_the_category() as $category ) {
+                        printf(
+                            '<a href="%s" class="special__category category-link category-%s">%s</a>',
+                            esc_url(get_category_link($category)),
+                            esc_html($category -> slug),
+                            esc_html($category -> name),
+                        ); 
+                    }
+                    ?>
+                    <h3 class="special__heading"><?php echo wp_trim_words( get_the_title(), 10); ?></h3>
+                    <p class="special__excerpt"><?php echo wp_trim_words( get_the_excerpt(), 15); ?></p>
+                    <a href="<?php the_permalink(); ?>" class="read-more-button special__button">
+                        <span>Читать далее</span>
+                        <svg class="arrow-icon">
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#arrow"></use>
+                        </svg>
+                    </a>
+                </li>
+                <?php
+                    }
+                } else {
+                    // Постов не найдено
+                }
+                // Возвращаем оригинальные данные поста. Сбрасываем $post.
+                wp_reset_postdata();
+                
+                ?>
+    
+                <!-- Еще 2 поста -->
+                
+                <?php                
+                global $post;
+
+                $myposts = get_posts([ 
+                    'numberposts' => 2,
+                ]);
+
+                if( $myposts ){
+                    foreach( $myposts as $post ){
+                        setup_postdata( $post );
+                        ?>
+                        <li class="articles-grid__item articles-grid-default">
+                            <a href="<?php the_permalink(); ?>" class="articles-grid-default__link">
+                                <h3 class="articles-grid-default__heading"><?php echo mb_strimwidth(get_the_title(), 0, 20, "..."); ?></h3>
+                                <p class="articles-grid-default__excerpt"><?php echo wp_trim_words( get_the_excerpt(), 7); ?></p>
+                                <span class="articles-grid-default__date"><?php the_time('j F'); ?></span>              
+                            </a>
+                        </li>                       
+                        <?php 
+                    }
+                } else {
+                    // Постов не найдено
+                }
+
+                wp_reset_postdata(); // Сбрасываем $post
+            ?>
+            </ul>         
         </div>
     </section>
 </main>
